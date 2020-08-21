6c5944f0b9285023bbb9223ad1347c991a34769e
Bhupesh999 <69840146+Bhupesh999@users.noreply.github.com>
2020-08-21 16:24:07 +0530

    Update and rename README.md to Pang ram.py

---
+++ Pang ram.py
@@ -0,0 +1,15 @@
+# pangram.py
+# check if the string is pang ram or not
+
+import string
+
+def is pang ram(str)
+    l="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for char in l:
+        if char not in str.upper():
+            return False
+
+    return True
+
+#programmer code
+string="
